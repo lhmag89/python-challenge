c53232f3d6c56c2228393cd734782e55359ed4a8
lhmag89 <119267098+lhmag89@users.noreply.github.com>
2022-12-21 22:36:35 -0800

    modified python file

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,24 +1,35 @@
+#import dependencies
 import os
 import csv
-
+#create relative filepath
 csvdata = os.path.join("Resources","budget_data.csv")
-
+#read csv file
 with open(csvdata) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    first_row = next(csvreader)
-    totmonths = 0
-    total = 0
+    
+    #store header row
+    header = next(csvreader)
+    
+    #convert columns of months and profit/loss to list and make a list for monthly changes
     months = []
     profit = []
     changes = []
+    
+    #use for loop to append values to lists
     for row in csvreader:
         profit.append(int(row[1]))
         months.append(row[0])
-        totmonths = totmonths + 1
-        total = total + int(row[1])
+    
+    #calculate number of months and total of profit/loss
+    totmonths = len(months)
+    total = sum(profit)
+    
+    #loop through profit to find changes per month and append results to a new list of monthly changes
     for x in range(1,totmonths):
         monthlyChange = profit[x]- profit[x-1]
         changes.append(int(monthlyChange))
+    
+    #run financial analysis calculations
     totalChange = sum(changes)
     aveChange = round(totalChange/len(changes),2)
     maxIncrease = max(changes)
@@ -26,15 +37,19 @@ with open(csvdata) as csvfile:
     maxDecrease = min(changes)
     minInd = changes.index(maxDecrease) + 1
 
+    #format multiline export
     text = f"""
     Financial Analysis
     _____________________________________________________
+
     Total Months: {totmonths}
     Total: ${total}
     Average Change: ${aveChange}
     Greatest Increase in Profits: {months[maxInd]} (${changes[maxInd-1]})
     Greatest Decrease in Profits: {months[minInd]} (${changes[minInd-1]})
+    _____________________________________________________
     """
+#export results
 print(text)
 file = open('analysis/financial_analysis.txt', 'w')
 file.write(text)
